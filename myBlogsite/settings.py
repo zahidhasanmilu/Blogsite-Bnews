87b0fdf95b66b661682f217cb5cedb997a2cdcaf
Zahid Hasan Milu <minions.milu889@gmail.com>
2023-11-30 03:15:52 +0600

    initial commit

--- myBlogsite/settings.py
+++ myBlogsite/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    #CUSTOME APPS    
+
+    # CUSTOME APPS
     'crispy_forms',
     'crispy_bootstrap5',
     'django_cleanup.apps.CleanupConfig',
